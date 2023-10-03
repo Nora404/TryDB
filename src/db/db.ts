3c483162c1c7ef6e5330532f54bb5426a28d4d08
Ina-Verena.Ludwig <ina.verena.ludwig@gmail.com>
2023-10-03 14:31:33 +0200

    Add Kommentare

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,6 @@
 import Dexie, {Table} from "dexie";
 
+// Für jede Tabelle wird ein Interface geschrieben
 export interface Player {
     id?: number;
     name: string;
@@ -20,20 +21,33 @@ export interface Item {
     vkPrice: number;
 }
 
+// -----------------------------------------------------------------------------------------
+
 export class AppDB extends Dexie {
+    // Das sind die Namen der einzelnen Tabellen
     player_DB!: Table<Player, number>;
     items_DB!: Table<Item, number>;
 
+    // Hier werden die Tabellen in die Datenbank aufgenommen
+    // Nach jeder Veränderung der Tabellen muss die version herhöht werden
+    // ++ = Auto-Increment Primary Key
+    // &  = Einzigartig
+    // *  = Mehrere Einträge, z.B ein Array
+    // https://dexie.org/docs/Version/Version.stores()
     constructor(){
         super('ngdexieliveQuery');
         this.version(2).stores({
+            // Alles was hier steht ist als Index zu sehen
+            // Nur nach den bekannten Indexies kann gesucht werden
             player_DB: '++id, hp, mana, gold',
             items_DB: '++id, playerId, name, typ, count, ekPrice, vkPrice',
         });
+        // Füllen der noch leeren Tabellen
         this.on('populate', ()=> this.populate());
     }
 
     async populate(){
+        // Mit add wird ein Eintrag der Tabelle hinzugefügt
         const playerId = await db.player_DB.add({
             name: 'Player',
             race: 'Mensch',
@@ -43,6 +57,7 @@ export class AppDB extends Dexie {
             gold: 50,
         });
         
+        // Mit blukAdd wird ein Array der Tabelle hinzugefügt
         await db.items_DB.bulkAdd([
             {
                 playerId,
@@ -64,6 +79,7 @@ export class AppDB extends Dexie {
     }
 
     async resetDB(){
+        // Macht genau das was der Name vermuten lässt
         await db.transaction('rw', 'player_DB', 'items_DB', ()=> {
           this.player_DB.clear();
           this.items_DB.clear();
@@ -72,4 +88,5 @@ export class AppDB extends Dexie {
       }
 }
 
+// Herrüber wird die Anwendung auf die Datenbank zugreifen können
 export const db = new AppDB();
