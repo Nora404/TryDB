3545e583a0fbad65c6d60a344782edfb11843f9f
Ina-Verena.Ludwig <ina.verena.ludwig@gmail.com>
2023-10-03 18:03:53 +0200

    Datenstruktur erstellt

--- src/db/db.ts
+++ src/db/db.ts
@@ -10,6 +10,10 @@ export interface Player {
     class: string;
     hp:    number;
     mana:  number;
+    str:   number;
+    sta:   number;
+    int:   number;
+    def:   number;
     gold:  number;
     skills:    string[]; // id des Skilles
     equipment: string[]; // id der Ausrüstung
@@ -31,6 +35,8 @@ export interface Skill {
     name:        string;
     typ:         string;
     element:     string;
+    need:        string;
+    kost:        number;
     force:       number;
     critForce:   number;
     critChance:  number;
@@ -49,7 +55,30 @@ export interface Equipment {
 }
 
 export interface Progress {
-    id?:   number;
-    name:  string;
-    value: any;
+    id?:      number;
+    name:     string;
+    category: string;
+    value:    any;
+}
+
+export class TryDB extends Dexie {
+    player_DB!: Table<Player, number>;
+    items_DB!: Table<Item, number>;
+    skills_DB!: Table<Skill, number>;
+    equipment_DB!: Table<Equipment, number>;
+    progress_DB!: Table<Progress, number>;
+
+    constructor(){
+        super('ngdexieliveQuery');
+        this.version(1).stores({
+            player_DB: '++id, name, hp, mana, str, sta, int, def, gold, *skills, *equipment, *progress, *items',
+            items_DB: '++id, name, typ, ekPrice, vkPrice',
+            skills_DB: '++id, name, typ, element, need, kost, force',
+            equipment_DB: '++id, name, typ, effect, power, ekPrice, vkPrice',
+            progress_DB: '++id, name, category',
+        });
+        this.on('populate', ()=> this.populate());
+    }
+
+    async populate(){}
 }
