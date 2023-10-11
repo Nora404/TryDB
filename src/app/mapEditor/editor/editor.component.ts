72d4e6f480a028e9d986e3421f0cac228ea67677
Ina-Verena.Ludwig <ina.verena.ludwig@gmail.com>
2023-10-11 20:36:06 +0200

    Karte hat grenzen

--- src/app/mapEditor/editor/editor.component.ts
+++ src/app/mapEditor/editor/editor.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Biom, biom, tryMap } from './biom';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, retry } from 'rxjs';
 
 @Component({
   selector: 'app-mapEditor',
@@ -12,19 +12,35 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class MapEditorComponent{
 
+  // Das ist die Karte, ein Mehrdimensionales Array
+  // [[1,2,3][1,2,3]] Diese Karte ist 2x3 Kacheln groß
   map = tryMap;
 
+  // Die Position der Karte, in Pixeln
+  // Es bestimmt den Startpunkt
   top: number = 0;
   left: number = 0;
-  size: number = 50;
-  playgroundWidth: number = 350;
-  playgroundHeight: number = 250;
 
-  // TODO Funktioniert noch nicht
-  minTop: number = 0;
-  minLeft: number = 0;
-  maxTop: number = this.playgroundHeight - this.size;
-  maxLeft: number = this.playgroundWidth - this.size;
+  // Die Größe der Kacheln, in Pixeln
+  size: number = 50;
+
+  // Die Größe des sichtbaren Spielfeldes
+  // der Multiplikator (ungerade) sind die gewünschten Kacheln
+  playgroundWidth: number = this.size * 7;
+  playgroundHeight: number = this.size * 5;
+
+  // Die Anzahl der Kacheln auf der gesamten Karte 
+  maxWidth: number = this.map.length;
+  maxHeight: number = this.map[0].length;
+
+  // Die Anzahl der Kacheln auf dem sichtbaren Spielfeld
+  tileWidth: number = this.playgroundWidth / this.size;
+  tileHeight: number = this.playgroundHeight / this.size;
+
+  // Die Kacheln (z.B. 4/5) genau in der Mitte des sichtbaren Spielfeldes
+  tileMiddelWidth: number = Math.round(this.tileWidth / 2);
+  tileMiddelHeigth: number = Math.round(this.tileHeight / 2);
+
 
   imgUrl: string = '../../../assets/mapIcons/';
 
@@ -36,8 +52,8 @@ export class MapEditorComponent{
   }
 
   cssMap = {
-    'grid-template-columns': 'repeat(' + tryMap.length + ',' + this.size + 'px)',
-    'grid-template-rows': 'repeat(' + tryMap[0].length + ',' + this.size + 'px)',
+    'grid-template-columns': 'repeat(' + this.map.length + ',' + this.size + 'px)',
+    'grid-template-rows': 'repeat(' + this.map[0].length + ',' + this.size + 'px)',
   }
 
   cssPlayer = {
@@ -60,20 +76,38 @@ export class MapEditorComponent{
   }
 
   west(){
-    this.left = this.left + this.size;
-    this.update();
+    const futureLeft =  this.left + this.size;
+
+    if(this.isPositionAllowed(this.top, futureLeft)){
+      this.left = this.left + this.size;
+      this.update();
+    }
   }
   nord(){
-    this.top = this.top + this.size;
-    this.update();
+    const futureTop = this.top + this.size;
+
+    if(this.isPositionAllowed(futureTop, this.left)){
+      this.top = futureTop;
+      this.update();
+    }
   }
   ost(){
-    this.left = this.left - this.size;
-    this.update();
+    const futureLeft = this.left - this.size; 
+
+    if(this.isPositionAllowed(this.top, futureLeft)){
+      this.left = futureLeft;
+      this.update();
+    }
   }
   sud(){
-    this.top = this.top - this.size;
-    this.update();
+    const futureTop = this.top - this.size;
+    console.log(futureTop)
+
+    if(this.isPositionAllowed(futureTop, this.left)){
+
+      this.top = futureTop;
+      this.update();
+    }
   }
 
   update(){
@@ -84,16 +118,43 @@ export class MapEditorComponent{
     };
   }
 
-  findMyPosition(){
-    const kachelnWidth = this.playgroundWidth / this.size;
-    const kachelnHeigth = this.playgroundHeight / this.size;
+  findMyPosition(top = this.top, left = this.left){
+    // Um den Index[?][?] zu finden werden zur besseren lesbarkeit Hilfsvariabeln erstellt
+    // Hilfsvariabel um den Pixelwert der mittleren Kachel zu finden 
+    const tileWidthPosition: number = this.size* this.tileMiddelWidth -this.size;
+    const tileHeigthPosition: number = this.size* this.tileMiddelHeigth -this.size;
 
-    let middelWidth = Math.round(kachelnWidth / 2);
-    let middelHeigth = Math.round(kachelnHeigth / 2);
+    // Nun wird die Position im Bezug zur ganzen Karte ermittelt
+    const relativeLeft: number = left - tileWidthPosition;
+    const relativeTop: number = top - tileHeigthPosition;
 
-    let playerPositionTop = Math.abs(this.top - (this.size*middelHeigth-this.size)) / this.size;
-    let playerPositionLeft = Math.abs(this.left - (this.size*middelWidth-this.size)) / this.size;
+    // Mit Math.abs nehme ich nur den positiven Wert
+    // Teile das durch this.size und ich habe die Kachel
+    let indexMiddleWidth: number = Math.abs(relativeLeft) / this.size;
+    let indexMiddleHeigth: number = Math.abs(relativeTop) / this.size;
 
-    return tryMap[playerPositionLeft][playerPositionTop];
+    return [indexMiddleWidth, indexMiddleHeigth];
+  }
+
+  getMyPositionTile(){
+    const index = this.findMyPosition();
+    return this.map[index[0]][index[1]]
+  }
+
+  isPositionAllowed(futureTop: number, futureLeft: number){
+    // Logik zur Bestimmung der zukünftigen Index-Position
+    const futureIndex = this.findMyPosition(futureTop, futureLeft);
+
+    if(futureIndex[0] < 0 || futureIndex[0] >= this.maxWidth) {
+      console.log('Horizontal außerhalb der Grenzen!');
+      return false;
+    }
+  
+    if(futureIndex[1] < 0 || futureIndex[1] >= this.maxHeight) {
+      console.log('Vertikal außerhalb der Grenzen!');
+      return false;
+    }
+
+    return true;
   }
 }
