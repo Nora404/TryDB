34681565c8be22a294e44533de52b421827dacfe
ina-verena <144120729+ina-verena@users.noreply.github.com>
2023-11-01 22:11:57 +0100

    etwas aufgeräumt

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -4,9 +4,6 @@ import { Biom, EmptyBiom } from '../db/biom';
 import { ActionComponent } from "./action/action.component";
 import { EventDialogComponent } from './event-dialog/event-dialog.component';
 import { LayoutService } from '../layout/layout.service';
-import { BehaviorSubject, of } from 'rxjs';
-import { tileEvent } from '../db/events';
-import { eventActions } from '../db/actions';
 
 @Component({
   selector: 'app-event',
@@ -21,43 +18,9 @@ export class EventComponent {
   @Input()
   set tile(tile: Biom | null) {
     this._tile = tile ? tile : EmptyBiom;
-    this._header$.next(tile?.name || 'Unbekannte Stimme');
-    this._text$.next(tile?.discription || 'Hallo?');
-    this._icon$.next(tile?.icon || 'nothing');
-    this._color$.next(tile?.color || [100, 100, 100]);
-    this._path$.next('map');
-    this._btn$.next([]);
   }
 
-  // tile: Biom | null = EmptyBiom;
-
-  @Input() coordinate: number[] = [0, 0];
   @Output() actionID: EventEmitter<number> = new EventEmitter<number>();
-
-  private _header$: BehaviorSubject<string> = new BehaviorSubject<string>('Unbekannte Stimme');
-  get header$() {
-    return this._header$.asObservable();
-  }
-  private _text$: BehaviorSubject<string> = new BehaviorSubject<string>('Hallo?');
-  get text$() {
-    return this._text$.asObservable();
-  }
-  private _icon$: BehaviorSubject<string> = new BehaviorSubject<string>('nothing');
-  get icon$() {
-    return this._icon$.asObservable();
-  }
-  private _color$: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([100, 100, 100]);
-  get color$() {
-    return this._color$.asObservable();
-  }
-  private _path$: BehaviorSubject<string> = new BehaviorSubject<string>('map');
-  get path$() {
-    return this._path$.asObservable();
-  }
-  private _btn$: BehaviorSubject<Array<{ button: string, actionID: number }>> = new BehaviorSubject<Array<{ button: string, actionID: number }>>([]);
-  get btn$() {
-    return this._btn$.asObservable();
-  }
 
   constructor(private layout: LayoutService) { }
 
@@ -69,36 +32,10 @@ export class EventComponent {
   }
 
   getStyleColors() {
-    return this.layout.getStyleColors(this._tile!, false);
+    return this.layout.getGrayColors();
   }
 
   executeAction(actionID: number) {
     this.actionID.emit(actionID);
-
-    console.log(actionID);
-
-
-    // if(actionID === 0 && this._tile){
-    //   console.log('Hallo!');
-
-    //   this._header$.next(this._tile.name);
-    //   this._text$.next(this._tile.discription);
-    //   this._color$.next(this._tile.color);
-    //   this._icon$.next(this._tile.icon);
-    //   this._path$.next('mapIcon')
-    //   this._btn$.next([]);
-    // }
-
-    // const newAction = eventActions.find(action => action.id === actionID)
-
-    // if(newAction){
-    //   this._header$.next(newAction.header);
-    //   this._text$.next(newAction.text);
-    //   this._color$.next([100,50,60]);
-    //   this._icon$.next(newAction.icon);
-    //   this._path$.next(newAction.path)
-    //   this._btn$.next(newAction.action);
-    // }
-
   }
 }
--- src/app/layout/layout.service.ts
+++ src/app/layout/layout.service.ts
@@ -8,16 +8,16 @@ export class LayoutService {
   private _imgUrl: string = '../assets/';
   private _imgNothing: string = '../assets/mapIcons/nothing.svg';
 
-  getStyleColors(biom: Biom, borderCheck: boolean = true){
+  getStyleColors(biom: Biom, borderCheck: boolean = true) {
     const color = biom.color;
-    const red: number = (color[0] || 100); 
-    const green: number = (color[1] || 100); 
-    const blue: number = (color[2] || 100); 
+    const red: number = (color[0] || 100);
+    const green: number = (color[1] || 100);
+    const blue: number = (color[2] || 100);
     const big = `4px solid rgb(${red * 0.5},${green * 0.5},${blue * 0.5})`;
     const small = `2px dotted rgb(${red * 0.5},${green * 0.5},${blue * 0.5})`;
 
     const style = {
-      'color':            `rgb(${red * 0.3},${green * 0.3},${blue * 0.3})`,
+      'color': `rgb(${red * 0.3},${green * 0.3},${blue * 0.3})`,
       'background-color': `rgb(${red},${green},${blue})`,
       'border-top': biom.nord && borderCheck ? small : big,
       'border-right': biom.ost && borderCheck ? small : big,
@@ -27,36 +27,45 @@ export class LayoutService {
     return style
   }
 
-  getRadialColors(color: number[] | undefined){
-    if(color){
+  getGrayColors() {
+    const style = {
+      'color': `rgb(30, 30, 30)`,
+      'border-color': `rgb(50, 50 ,50)`,
+      'background-color': `rgb(100, 100, 100)`,
+    }
+    return style
+  }
+
+  getRadialColors(color: number[] | undefined) {
+    if (color) {
       return {
         'background': `radial-gradient(circle, 
             rgba(${color[0]}, ${color[1]}, ${color[2]}, 1) 0%, 
             rgba(${color[0] * 0.8}, ${color[1] * 0.8}, ${color[2] * 0.8}, 1) 100%)`,
-        'border' : `5px solid rgb(${color[0] * 0.4}, ${color[1] * 0.4}, ${color[2] * 0.4})`,   
-        'height' : '100%',
+        'border': `5px solid rgb(${color[0] * 0.4}, ${color[1] * 0.4}, ${color[2] * 0.4})`,
+        'height': '100%',
       }
     }
     return
   }
 
-  getImgUrlMap(name: string | undefined){
-    return name ? this._imgUrl + '/mapIcons/' + name + '.svg' : this._imgNothing; 
+  getImgUrlMap(name: string | undefined) {
+    return name ? this._imgUrl + '/mapIcons/' + name + '.svg' : this._imgNothing;
   }
-  getImgUrlCreature(name: string | undefined){
-    return name ? this._imgUrl + '/kreatur/' + name + '.svg' : this._imgNothing; 
+  getImgUrlCreature(name: string | undefined) {
+    return name ? this._imgUrl + '/kreatur/' + name + '.svg' : this._imgNothing;
   }
-  getImgUrlPlayer(name: string | undefined){
-    return name ? this._imgUrl + '/player/' + name + '.svg' : this._imgNothing; 
+  getImgUrlPlayer(name: string | undefined) {
+    return name ? this._imgUrl + '/player/' + name + '.svg' : this._imgNothing;
   }
-  getImgUrlUiIcons(name: string | undefined){
-    return name ? this._imgUrl + '/uiIcons/' + name + '.svg' : this._imgNothing; 
+  getImgUrlUiIcons(name: string | undefined) {
+    return name ? this._imgUrl + '/uiIcons/' + name + '.svg' : this._imgNothing;
   }
 
-  getPlayerStyle(size: number){
+  getPlayerStyle(size: number) {
     const style = {
-      'width': size * 0.9 +'px',
-      'height': size * 0.9 +'px',
+      'width': size * 0.9 + 'px',
+      'height': size * 0.9 + 'px',
     }
     return style
   }
